cc7c4ccf89de4557fe5ca0503e40bd32e680a416
Atahan Halıcı <atahanhalici@gmail.com>
2025-04-20 04:48:45 +0300

    route page fake api service added

--- lib/pages/routesPage/routes_page_view_model.dart
+++ lib/pages/routesPage/routes_page_view_model.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:rota_erzincan/core/base/base_view_model.dart';
 import 'package:rota_erzincan/models/RouteItem.dart';
+import 'package:rota_erzincan/services/api_service.dart';
 
 class RoutesPageViewModel extends ChangeNotifier with BaseViewModel {
-  //final ApiService _apiService = ApiService();
+  final ApiService _apiService = ApiService();
   List<RouteItem> routeItems = [];
   bool isLoading = true;
 
-  List<Map<String, dynamic>> _userRoutes = [
+  final List<Map<String, dynamic>> _userRoutes = [
     {
       'id': '1',
       'name': 'Kemaliye - Karanlık Kanyon',
@@ -47,61 +48,11 @@ class RoutesPageViewModel extends ChangeNotifier with BaseViewModel {
     isLoading = true;
     notifyListeners(); // shimmer başlasın
 
-    //categoryItems = await _apiService.fetchAllCategories();
-    routeItems = [
-      RouteItem(
-        title: "Tarihin İçinden Rotası",
-        subtitle: "Erzincan'ın tarihi ve kültürel zenginliklerini keşfedin.",
-        imageUrl:
-            "https://picsum.photos/id/1011/600/400", // Tarihi yerler havası
-        icon: Icons.account_balance, // Tarihi bina simgesi
-      ),
-      RouteItem(
-        title: "Çocuğumla Geziyorum Rotası",
-        subtitle: "Ailece eğlenebileceğiniz parklar ve etkinlikler.",
-        imageUrl: "https://picsum.photos/id/1027/600/400", // Çocuk teması
-        icon: Icons.child_friendly, // Çocuk dostu simgesi
-      ),
-      RouteItem(
-        title: "Doğadan Esintiler Rotası",
-        subtitle: "Doğayla iç içe huzurlu rotaları keşfedin.",
-        imageUrl: "https://picsum.photos/id/1043/600/400", // Doğa manzarası
-        icon: Icons.nature_people, // Doğa yürüyüşü ikonu
-      ),
-      RouteItem(
-        title: "Sporcunun Dostu Rotası",
-        subtitle: "Aktif yaşamı sevenler için ideal parkurlar.",
-        imageUrl: "https://picsum.photos/id/1052/600/400", // Spor yapma alanı
-        icon: Icons.fitness_center, // Spor ikonu
-      ),
-      RouteItem(
-        title: "Erzincan ve Macera Rotası",
-        subtitle: "Adrenalin ve keşif dolu bir Erzincan deneyimi.",
-        imageUrl: "https://picsum.photos/id/1062/600/400", // Macera teması
-        icon: Icons.explore, // Keşif simgesi
-      ),
-    ];
+    routeItems = await _apiService.fetchAllRoutes();
 
     isLoading = false;
     notifyListeners(); // shimmer dursun, liste gözüksün
   }
-
-  /* Future<void> initialize() async {
-    await _loadRoutes();
-  }
-
-  Future<void> _loadRoutes() async {
-    _contentItems = await _apiService.getContents(routes);
-
-    _isLoading = false;
-
-    // 👇 Bu notify çalışmasına rağmen UI değişmiyorsa viewModel'i StatefulWidget içinde State'e taşımadın demektir
-    notifyListeners();
-  }
-
-  Future<void> navigateToPage(CategoryContentItem item) async {
-    navigationService.navigateToDetailsPage(item);
-  }*/
 
   @override
   void dispose() {
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,6 +7,7 @@ import 'package:rota_erzincan/models/FacilityModel.dart';
 import 'package:rota_erzincan/models/FeatureModel.dart';
 import 'package:rota_erzincan/models/InfoCardModel.dart';
 import 'package:rota_erzincan/models/PhotoModel.dart';
+import 'package:rota_erzincan/models/RouteItem.dart';
 
 class ApiService {
   Future<List<CategoryModel>> fetchCategories() async {
@@ -151,6 +152,55 @@ class ApiService {
     // Map'ten CategoryItem listesine dönüştür
     return responseData.map((data) {
       return CategoryItem(
+        title: data['title'],
+        subtitle: data['subtitle'],
+        imageUrl: data['imageUrl'],
+        icon: data['icon'],
+      );
+    }).toList();
+  }
+
+  Future<List<RouteItem>> fetchAllRoutes() async {
+    await Future.delayed(const Duration(milliseconds: 500)); // sahte gecikme
+
+    final List<Map<String, dynamic>> responseData = [
+      {
+        "title": "Tarihin İçinden Rotası",
+        "subtitle": "Erzincan'ın tarihi ve kültürel zenginliklerini keşfedin.",
+        "imageUrl":
+            "https://picsum.photos/id/1011/600/400", // Tarihi yerler havası
+        "icon": Icons.account_balance,
+      } // Tarihi bina simgesi
+      ,
+      {
+        "title": "Çocuğumla Geziyorum Rotası",
+        "subtitle": "Ailece eğlenebileceğiniz parklar ve etkinlikler.",
+        "imageUrl": "https://picsum.photos/id/1027/600/400", // Çocuk teması
+        "icon": Icons.child_friendly, // Çocuk dostu simgesi
+      },
+      {
+        "title": "Doğadan Esintiler Rotası",
+        "subtitle": "Doğayla iç içe huzurlu rotaları keşfedin.",
+        "imageUrl": "https://picsum.photos/id/1043/600/400", // Doğa manzarası
+        "icon": Icons.nature_people, // Doğa yürüyüşü ikonu
+      },
+      {
+        "title": "Sporcunun Dostu Rotası",
+        "subtitle": "Aktif yaşamı sevenler için ideal parkurlar.",
+        "imageUrl": "https://picsum.photos/id/1052/600/400", // Spor yapma alanı
+        "icon": Icons.fitness_center, // Spor ikonu
+      },
+      {
+        "title": "Erzincan ve Macera Rotası",
+        "subtitle": "Adrenalin ve keşif dolu bir Erzincan deneyimi.",
+        "imageUrl": "https://picsum.photos/id/1062/600/400", // Macera teması
+        "icon": Icons.explore, // Keşif simgesi
+      }
+    ];
+
+    // Map'ten CategoryItem listesine dönüştür
+    return responseData.map((data) {
+      return RouteItem(
         title: data['title'],
         subtitle: data['subtitle'],
         imageUrl: data['imageUrl'],
